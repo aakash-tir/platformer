cc971120f786789b8875f234857ccd95f106e322
aakash-tir <aakashtirathdas@gmail.com>
2025-02-20 11:06:35 -0800

    attempted to code the camera movement
    could not successfully code the camera movement so will move on to other items till then

--- Assets/PlayerControls.cs
+++ Assets/PlayerControls.cs
@@ -1,3 +1,4 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour{
@@ -5,19 +6,21 @@ public class PlayerController : MonoBehaviour{
     [SerializeField] private InputManager inputManager;
     [SerializeField] private float speed;
     [SerializeField] private float jump;
-    [SerializeField] private float JumpCounter;
+    
+    [SerializeField] private CinemachineCamera freeLookCamera;
 
     private Rigidbody rb;
+    private float JumpCounter;
     void Start(){
         inputManager.OnMove.AddListener(MovePlayer);
         inputManager.Jump.AddListener(onJump);
         rb=GetComponent<Rigidbody>();
         
     }
-    void Update()
-    {
-     //transform.rotation = Quaternion.Euler(0,transform.rotation.eulerAngles.y,0);   
-    }
+    /*void Update(){
+        transform.forward = freeLookCamera.transform.forward;
+        transform.rotation = Quaternion.Euler(0,transform.rotation.eulerAngles.y,0);
+    }*/
 
     private void MovePlayer(Vector3 direction){
         Vector3 moveDirection = new(direction.x, direction.y, direction.z);
